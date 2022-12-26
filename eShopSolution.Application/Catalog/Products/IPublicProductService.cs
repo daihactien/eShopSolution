a5f341c580267079989991e879e35e92a5dd5ffe
Nguyen Quang Hien <19dh111085@st.huflit.edu.vn>
2022-12-26 10:15:56 +0700

    Create application structure

---
+++ eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,14 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        public PagedViewModel<ProductViewModel>
+            GetAllByCategoryId(int categoryId, int pageIndex, int pageSize);
+    }
+}
